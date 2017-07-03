a49210f648036d01d4072a5cc0ec23f45a73cdb1
cypherix93 <bikram.ghosh93@gmail.com>
2017-07-03 02:20:56 -0400

    Added jquery slim

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,5 +1,7 @@
 import 'marked';
-import 'zepto/src/zepto';
+const jquery = require('jquery/dist/jquery.slim');
+
+(window as any).$ = (window as any).jQuery = jquery;
 
 const {Router} = require('director/build/director.js');
 (window as any).Router = Router;
