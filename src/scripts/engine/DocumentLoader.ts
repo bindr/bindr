7304535f943668ce5c88fb2388a88af840ec58b4
cypherix93 <bikram.ghosh93@gmail.com>
2017-07-03 23:05:01 -0400

    Who needs path helper?

--- src/scripts/engine/DocumentLoader.ts
+++ src/scripts/engine/DocumentLoader.ts
@@ -1,9 +1,10 @@
 import superagent from 'superagent';
 
-import {getDocFilePath} from '../helpers/PathsHelper';
+import {Document, ManifestEntry, ManifestEntryType, Section} from '../models/Manifest';
+import {getManifest} from '../config/Manifest';
 import {parseMarkdown} from './MarkdownParser';
 
-export async function getDocumentHtml(path: string) {
+export async function getDocumentHtml(path: string): Promise<string> {
     const filePath = await getDocFilePath(path);
 
     // TODO: Handle filePath undefined
@@ -16,4 +17,42 @@ export async function getDocumentHtml(path: string) {
     const htmlText = parseMarkdown(markdownText);
 
     return htmlText;
+}
+
+let docsUrlMap: { [key: string]: string };
+
+type DocsUrlMap = typeof docsUrlMap;
+
+async function getDocFilePath(urlPath: string): Promise<string> {
+    if (!docsUrlMap) {
+        await buildUrlMap();
+    }
+
+    // Remove leading slash
+    urlPath = urlPath.substring(1);
+
+    return docsUrlMap[urlPath];
+}
+
+async function buildUrlMap() {
+    const manifest = await getManifest();
+    docsUrlMap = recurseOverManifest(manifest);
+}
+
+function recurseOverManifest(entry: ManifestEntry): DocsUrlMap {
+    const files: DocsUrlMap = {};
+
+    if (entry.type === ManifestEntryType.Document) {
+        let doc = entry as Document;
+        files[doc.url] = doc.filePath;
+    }
+    else if (entry.type === ManifestEntryType.Section) {
+        let section = entry as Section;
+        for (let child of section.children) {
+            let childrenFiles = recurseOverManifest(child);
+            Object.assign(files, childrenFiles);
+        }
+    }
+
+    return files;
 }
