72f88dabe0a6b186aa0e5275943d571ccc6e42ac
cypherix93 <bikram.ghosh93@gmail.com>
2017-07-04 00:16:11 -0400

    Started rendering side navbar.

--- src/components/App.ts
+++ src/components/App.ts
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 
 import TopNav from './layout/TopNav/TopNav.vue';
+import SideNav from './layout/SideNav/SideNav.vue';
 
 @Component({
     components: {
-        TopNav
+        TopNav,
+        SideNav
     }
 })
 export default class extends Vue {
--- src/components/App.vue
+++ src/components/App.vue
@@ -5,7 +5,7 @@
         <div class="container bindr-root">
             <div class="row">
                 <div class="col-xs-12 col-md-4 col-lg-3">
-                    <!--<bindr-nav></bindr-nav>-->
+                    <SideNav></SideNav>
                 </div>
                 <div class="col-xs-12  col-md-8 col-lg-9">
                     <div id="router-container">
