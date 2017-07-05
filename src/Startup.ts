dbda21d940a12fb5b9bbfdef0fd2093c80e8d4ed
cypherix93 <bikram.ghosh93@gmail.com>
2017-07-04 20:57:47 -0400

    Screwing around with vue material.

--- src/Startup.ts
+++ src/Startup.ts
@@ -2,8 +2,10 @@ import './scripts/hooks/ComponentHooks';
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueMaterial from 'vue-material';
 
 import {initBindr} from './scripts/Bindr';
+import {initThemes} from './scripts/theme/Default';
 import {loadConfig} from './scripts/config/Config';
 import {loadManifest} from './scripts/config/Manifest';
 
@@ -11,12 +13,16 @@ export default async function startup() {
 
     // Bootstrap Vue Modules
     Vue.use(VueRouter);
+    Vue.use(VueMaterial);
 
     // Load Bindr related configs
     await Promise.all([
         loadConfig(),
         loadManifest()
     ]);
+
+    // Init Themes
+    await initThemes();
 
     // Initialize Bindr
     await initBindr();
--- src/types/Global.d.ts
+++ src/types/Global.d.ts
@@ -1,5 +1,11 @@
 declare let __webpack_public_path__: string;
 
+// Vue Material
+declare module 'vue-material' {
+    import {PluginObject} from 'vue';
+    export default null as PluginObject<any>;
+}
+
 // Vue files fix
 declare module '*.vue' {
     import Vue from 'vue';
